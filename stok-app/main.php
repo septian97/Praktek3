11c75dd62bbd0f8b5aefc5f8f1711dc2f2a87efa
septian97 <septian.rizaldi@yahoo.com>
2018-03-21 16:19:53 +0700

    upload2

--- stok-app/main.php
+++ stok-app/main.php
@@ -1,5 +1,6 @@
 <h2>Aplikasi Stok Barang</h2>
 <hr>
+<a href="input.php">Tambah Data</a>
 <table border="1">
     <tr>
         <th>KODE</th>
@@ -7,9 +8,7 @@
         <th>STOK<th>
     
     <tr>
-
-</table>
-<?php
+    <?php
     include "koneksi.php";
 
     $konkesiObj = new Koneksi();
@@ -17,13 +16,32 @@
         
         
     if($koneksi->connect_error) {
-       echo "<tr><td>";
-       echo "Gagal Koneksi : " . $koneksi->connect_error;
-       echo "</td></tr>";
+        echo "<tr><td>";
+        echo "Gagal Koneksi : " . $koneksi->connect_error;
+        echo "</td></tr>";
     } //else {
 //        echo "<tr><td>";
 //        echo "Sambungan basis data Berhasil";
 //        echo "</td></tr>";
 //    }
-$query = "select * from stok_barang";
+
+    $query = "select * from stok_barang";
+    $data = $koneksi->query($query);
+
+    if($data->num_rows <=0){
+        echo "<tr><td>";
+        echo "DATA NIHIL";
+        echo "</tr></td>";
+    } else { 
+       while($row = $data->fetch_assoc()){
+            echo "<tr>";
+            echo "<td>" . $row["kode"] . "</td>";
+            echo "<td>" . $row["nama_barang"] . "</td>"; 
+            echo "<td>" . $row["stok"] . "</td>"; 
+            echo "</tr>";
+       }
+
+    }
 ?>
+
+</table>
